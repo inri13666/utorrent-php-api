e2b24ad2adf3e6b27537bd6102c3b0f72d704525
Nikita Makarov <nikita.makarov@effective-soft.com>
2015-07-20 16:57:38 +0300

    FIXED PSR-0

--- demo.php
+++ demo.php
@@ -5,8 +5,8 @@
  * Time  : 2:22 PM
  * E-Mail: nikita.makarov@effective-soft.com
  */
-require_once 'lib/uTorrent.php';
-require_once 'lib/Queue/Torrent.php';
+require_once 'lib/Akuma/uTorrent.php';
+require_once 'lib/Akuma/Queue/Torrent.php';
 
 $service = new \Akuma\uTorrent('127.0.0.1','13666','admin','123456');
 var_dump($service->isOnline());
